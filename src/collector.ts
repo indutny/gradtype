de17b07aef2056b406f491d9e5e3fb85f9805505
Fedor Indutny <fedor@indutny.com>
2018-03-25 13:25:27 -0400

    collector: update

--- src/collector.ts
+++ src/collector.ts
@@ -9,53 +9,33 @@ interface ICollectorEvent {
   readonly code: number;
 }
 
-export interface ICollectorResult {
-  readonly delta: number;
-  readonly fromCode: number;
-  readonly fromTS: Timestamp;
-  readonly toCode: number;
-  readonly toTS: Timestamp;
+interface ICollectorResult {
+  readonly timestamp: number;
+  readonly code: number;
 }
 
 export class Collector {
-  private readonly events: (ICollectorEvent | undefined)[] = [];
+  private readonly start: Timestamp = process.hrtime();
+  private readonly events: (ICollectorEvent | 'reset')[] = [];
 
   public register(code: number): void {
-    this.events.push({ timestamp: process.hrtime(), code });
+    this.events.push({ timestamp: process.hrtime(this.start), code });
   }
 
   public reset(): void {
-    this.events.push(undefined);
+    this.events.push('reset');
   }
 
-  public getResult(): ReadonlyArray<ICollectorResult> {
-    const result: ICollectorResult[] = [];
-
-    let lastEvent: ICollectorEvent | undefined;
-    for (const event of this.events) {
-      // reset
-      if (event === undefined) {
-        lastEvent = undefined;
-        continue;
+  public getResult(): ReadonlyArray<ICollectorEvent | 'reset'> {
+    return this.events.map((event) => {
+      if (event === 'reset') {
+        return 'reset';
+      } else {
+        return {
+          timestamp: event.timestamp[0] + event.timestamp[1] * 1e-9,
+          code: event.code,
+        };
       }
-
-      if (lastEvent === undefined) {
-        lastEvent = event;
-        continue;
-      }
-
-      const delta = (event.timestamp[0] - lastEvent.timestamp[0]) +
-        1e-9 * (event.timestamp[1] - lastEvent.timestamp[1]);
-
-      result.push({
-        fromTS: lastEvent.timestamp,
-        toTS: event.timestamp,
-        delta,
-        fromCode: lastEvent.code,
-        toCode: event.code,
-      });
-      lastEvent = event;
-    }
-    return result;
+    });
   }
 }
