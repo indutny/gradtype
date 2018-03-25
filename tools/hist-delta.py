2cfa3a559871a9e3f1895db8d72bebf969aa2888
Fedor Indutny <fedor@indutny.com>
2018-03-25 18:50:06 -0400

    datasets: zladuric

--- tools/hist-delta.py
+++ tools/hist-delta.py
@@ -13,7 +13,7 @@ with open(sys.argv[1]) as input:
   # the histogram of the data
   plt.hist(delta, 1500, facecolor='g', alpha=0.75)
 
-  plt.axis([ 0, 500, 0, 800 ])
+  plt.axis([ 0, 500, 0, 200 ])
   plt.xlabel('Delta')
   plt.ylabel('Count')
   plt.grid(True)
