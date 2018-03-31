36ba477d265900dba881abc1ea9a7f8b8538dbb2
Fedor Indutny <fedor@indutny.com>
2018-03-31 03:12:58 -0400

    datasets: re-add labels for python

--- bin/dataset.ts
+++ bin/dataset.ts
@@ -17,7 +17,11 @@ const OUT_DIR = path.join(__dirname, '..', 'out');
 
 const labels: string[] = fs.readdirSync(DATASETS_DIR)
   .filter((file) => /\.json$/.test(file))
-  .map((file) => file.replace(/\.json$/, ''));
+  .map((file) => file.replace(/\.json$/, ''))
+  .filter((file) => file !== 'index');
+
+fs.writeFileSync(path.join(DATASETS_DIR, 'index.json'), JSON.stringify(
+  labels, null, 2));
 
 function encodeSequence(sequence) {
   totalSequences++;
