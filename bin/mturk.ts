c2a135874f8e83533cef3bb9a75379de4d19ed9b
Fedor Indutny <fedor@indutny.com>
2018-03-31 02:34:49 -0400

    datasets: add data from mturk

---
+++ bin/mturk.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env npx ts-node
+
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const DATASETS_DIR = path.join(__dirname, '..', 'datasets');
+const MTURK_DIR = path.join(__dirname, '..', 'mturk', 'datasets');
+const OUT_DIR = path.join(__dirname, '..', 'out');
+
+const files: string[] = fs.readdirSync(MTURK_DIR)
+  .filter((file) => /\.json$/.test(file))
+.map((file) => file.replace(/\.json$/, ''));
+
+files.forEach((name) => {
+  const content = fs.readFileSync(path.join(MTURK_DIR, name + '.json'));
+  fs.writeFileSync(path.join(DATASETS_DIR,
+    'sv-' + name.slice(0, 8) + '.json'));
+});
