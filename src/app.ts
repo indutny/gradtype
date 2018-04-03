cc75a730f014381b73ed20c8c6220db3eb5c1fc0
Fedor Indutny <fedor@indutny.com>
2018-04-03 14:43:35 -0400

    app: increase sentence count

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import leven = require('leven');
 
 const API_ENDPOINT = 'https://gradtype-survey.darksi.de/';
 const LS_KEY = 'gradtype-survey-v1';
-const INITIAL_COUNTER = 23;
+const INITIAL_COUNTER = 60;
 const TOLERANCE = 0.5;
 
 const elems = {
