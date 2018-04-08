69156e1409c5a830a25f01dfafdc1ede46c79ece
Fedor Indutny <fedor@indutny.com>
2018-04-08 18:08:28 -0400

    tf: save progress on model

---
+++ tf/model.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+
+# Internal
+import dataset
+
+EMBED_WIDTH = 7
+DENSE_PRE_COUNT = 3
+DENSE_PRE_WIDTH = 32
+GRU_WIDTH = 128
+DENSE_POST_COUNT = 1
+DENSE_POST_WIDTH = 128
+FEATURE_COUNT = 128
+
+class Embedding():
+  def __init__(self, name, max_code, width):
+    self.width = width
+    with tf.variable_scope('layer/' + name):
+      self.weights = tf.get_variable('weights', (max_code, width))
+
+  def apply(self, codes):
+    return tf.gather(self.weights, codes)
+
+class Model():
+  def __init__(self):
+    self.l2 = tf.contrib.layers.l2_regularizer(0.001)
+
+    self.embedding = Embedding('embedding', dataset.MAX_CHAR + 2, EMBED_WIDTH)
+
+    self.pre = []
+    for i in range(0, DENSE_PRE_COUNT):
+      self.pre.append(tf.layers.Dense(name='dense_pre_{}'.format(i),
+                                      units=DENSE_PRE_WIDTH,
+                                      activation=tf.nn.selu,
+                                      kernel_regularizer=self.l2))
+
+    self.gru = tf.contrib.rnn.GRUCell(name='gru', num_units=GRU_WIDTH)
+    self.features = tf.layers.Dense(name='features',
+                                    units=FEATURE_COUNT,
+                                    kernel_regularizer=self.l2)
+
+    self.post = []
+    for i in range(0, DENSE_POST_COUNT):
+      self.post.append(tf.layers.Dense(name='dense_post_{}'.format(i),
+                                       units=DENSE_POST_WIDTH,
+                                       activation=tf.nn.selu,
+                                       kernel_regularizer=self.l2))
+
+  def __call__(self, codes, deltas):
+    batch_size = codes.shape[0]
+    sequence_len = codes.shape[1]
+
+    embedding = self.embedding.apply(codes)
+    deltas = tf.expand_dims(deltas, axis=-1)
+    series = tf.concat([ deltas, embedding ], axis=-1)
+
+    state = self.gru.zero_state(batch_size, tf.float32)
+    x = None
+    for i in range(0, sequence_len):
+      frame = series[:, i]
+      for pre in self.pre:
+        frame = pre(frame)
+
+      state, x = self.gru(frame, state)
+
+    for post in self.post:
+      x = post(x)
+
+    x = self.features(x)
+
+    return x
--- tf/train.py
+++ tf/train.py
@@ -2,5 +2,18 @@ import tensorflow as tf
 
 # Internal
 import dataset
+from model import Model
 
 train, validate = dataset.load()
+
+model = Model()
+
+with tf.Session() as sess:
+  codes = tf.constant([ [ 1, 2, 3 ] ], dtype=tf.int32)
+  deltas = tf.constant([ [ 0.1, 0.2, 0.3 ] ], dtype=tf.float32)
+
+  concat = model(codes, deltas)
+
+  sess.run(tf.global_variables_initializer())
+
+  print(sess.run(concat))
