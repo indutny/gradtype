0b761c5bc32290c2f5115b94dd33afd4c4c9c14c
Fedor Indutny <fedor@indutny.com>
2018-04-13 17:43:43 -0600

    tf: conv regression

--- tf/train-regression-conv.py
+++ tf/train-regression-conv.py
@@ -11,8 +11,6 @@ from model import Model
 RUN_NAME = os.environ.get('GRADTYPE_RUN')
 if RUN_NAME is None:
   RUN_NAME = time.asctime()
-RESTORE_FROM = os.environ.get('GRADTYPE_RESTORE')
-
 LOG_DIR = os.path.join('.', 'logs', RUN_NAME)
 
 # Maximum number of epochs to run for
@@ -21,9 +19,6 @@ MAX_EPOCHS = 500000
 # Validate every epoch:
 VALIDATE_EVERY = 5
 
-# Number of sequences per category in batch
-BATCH_SIZE = 32
-
 # Learning rate
 LR = 0.001
 
@@ -31,7 +26,9 @@ LR = 0.001
 # Load dataset
 #
 
-train_dataset, validate_dataset = dataset.load()
+train_dataset, validate_dataset = dataset.load(mode='regression')
+train_dataset = dataset.flatten_dataset(train_dataset)
+validate_dataset = dataset.flatten_dataset(validate_dataset)
 
 #
 # Initialize model
@@ -41,13 +38,13 @@ input_shape = (None, dataset.MAX_SEQUENCE_LEN,)
 
 codes = tf.placeholder(tf.int32, shape=input_shape, name='codes')
 deltas = tf.placeholder(tf.float32, shape=input_shape, name='deltas')
-category_count = tf.placeholder(tf.int32, shape=(), name='category_count')
+categories = tf.placeholder(tf.int32, shape=(None,), name='categories')
 training = tf.placeholder(tf.bool, shape=(), name='training')
 
 model = Model(training=training)
 
 output = model.build_conv(codes, deltas)
-t_metrics = model.get_metrics(output, category_count, BATCH_SIZE)
+t_metrics = model.get_regression_metrics(output, categories)
 
 #
 # Initialize optimizer
@@ -74,16 +71,12 @@ def log_summary(prefix, metrics, step):
 saver = tf.train.Saver(max_to_keep=10000, name=RUN_NAME)
 
 with tf.Session() as sess:
-  if RESTORE_FROM != None:
-    print('Restoring from "{}"'.format(RESTORE_FROM))
-    saver.restore(sess, RESTORE_FROM)
-
   sess.run(tf.global_variables_initializer())
 
   step = 0
   for epoch in range(0, MAX_EPOCHS):
-    train_batches = dataset.gen_hard_batches(train_dataset, \
-        batch_size=BATCH_SIZE)
+    train_batches = dataset.gen_regression(train_dataset)
+    validate_batches = dataset.gen_regression(validate_dataset)
 
     saver.save(sess, LOG_DIR, global_step=step)
     print('Epoch {}'.format(epoch))
@@ -92,38 +85,31 @@ with tf.Session() as sess:
       _, metrics, reg_loss = sess.run(tensors, feed_dict={
         codes: batch['codes'],
         deltas: batch['deltas'],
-        category_count: len(train_dataset),
+        categories: batch['categories'],
         training: True,
       })
       metrics['regularization_loss'] = reg_loss
-
       log_summary('train', metrics, step)
+
       step += 1
-    writer.flush()
-
-    if epoch % VALIDATE_EVERY != 0:
-      continue
-
-    validate_batches = dataset.gen_hard_batches(validate_dataset, \
-        batch_size=BATCH_SIZE)
 
     print('Validation...')
     mean_metrics = None
     for batch in validate_batches:
-      v_metrics = sess.run(t_metrics, feed_dict={
+      metrics = sess.run(t_metrics, feed_dict={
         codes: batch['codes'],
         deltas: batch['deltas'],
-        category_count: len(validate_dataset),
+        categories: batch['categories'],
         training: False,
       })
 
       if mean_metrics is None:
         mean_metrics = {}
-        for key in v_metrics:
+        for key in metrics:
           mean_metrics[key] = []
 
-      for key in v_metrics:
-        mean_metrics[key].append(v_metrics[key])
+      for key in metrics:
+        mean_metrics[key].append(metrics[key])
 
     for key in mean_metrics:
       mean_metrics[key] = np.mean(mean_metrics[key])
