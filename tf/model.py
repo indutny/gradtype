02f6bfc9fd9a9dbd240471ec5ea028ce96981011
Fedor Indutny <fedor@indutny.com>
2018-04-27 14:48:52 -0400

    Revert "model: disable batch norm"
    This reverts commit de0ccc5b65f1c813c935e42c7b7306f90fe8d7dd.

--- tf/model.py
+++ tf/model.py
@@ -151,6 +151,7 @@ class Model():
                                 kernel_regularizer=self.l2,
                                 activation=tf.nn.selu,
                                 dilation_rate=dilation_rate)
+      series = tf.layers.batch_normalization(series)
       sequence_len -= dilation_rate * 3
       dilation_rate *= 2
 
