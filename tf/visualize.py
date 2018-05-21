bcd3fa1d55e08e85a65f42cdfb1c3067649c4778
Fedor Indutny <fedor@indutny.com>
2018-05-21 01:06:50 -0400

    visualize: fix

--- tf/visualize.py
+++ tf/visualize.py
@@ -74,10 +74,11 @@ model = Model(training=False)
 
 input_shape = (None, dataset.MAX_SEQUENCE_LEN,)
 
+p_types = tf.placeholder(tf.float32, shape=input_shape, name='types')
 p_codes = tf.placeholder(tf.int32, shape=input_shape, name='codes')
 p_deltas = tf.placeholder(tf.float32, shape=input_shape, name='deltas')
 
-output = model.build(p_codes, p_deltas)
+output = model.build(p_types, p_codes, p_deltas)
 
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
@@ -100,18 +101,22 @@ with tf.Session() as sess:
   train_dataset, _ = dataset.flatten_dataset(train_dataset)
   validate_dataset, _ = dataset.flatten_dataset(validate_dataset)
 
+  types = []
   codes = []
   deltas = []
 
   for seq in train_dataset:
+    types.append(seq['types'])
     codes.append(seq['codes'])
     deltas.append(seq['deltas'])
 
   for seq in validate_dataset:
+    types.append(seq['types'])
     codes.append(seq['codes'])
     deltas.append(seq['deltas'])
 
   features = sess.run(output, feed_dict={
+    p_types: types,
     p_codes: codes,
     p_deltas: deltas,
   })
