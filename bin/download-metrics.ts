fa0d1d9ad8ff822404d93de7926063423a66af6e
Fedor Indutny <fedor@indutny.com>
2018-06-24 12:48:50 -0400

    tools: chart-metrics

---
+++ bin/download-metrics.ts
@@ -0,0 +1,77 @@
+#!/usr/bin/env npx ts-node
+
+import fetch from 'node-fetch';
+
+const HOST = process.env.GRADTYPE_HOST || '127.0.0.1';
+const RUN = process.env.GRADTYPE_RUN || 'ds396-cleanup-lr_0.001';
+const PORT = 6006;
+
+const BASE_URI =
+  `http://${HOST}:${PORT}/data/plugin/scalars/scalars?run=${RUN}&format=json`;
+
+async function downloadSingle(tag) {
+  const uri = `${BASE_URI}&tag=${encodeURIComponent(tag)}`;
+
+  const res = await fetch(uri);
+  return await res.json();
+}
+
+async function download() {
+  const metrics = [
+    'negative_5',
+    'negative_10',
+    'negative_25',
+    'negative_50',
+    'negative_75',
+    'negative_90',
+    'negative_95',
+    'positive_5',
+    'positive_10',
+    'positive_25',
+    'positive_50',
+    'positive_75',
+    'positive_90',
+    'positive_95',
+  ];
+
+  const tags = [];
+  for (const category of [ 'train', 'validate' ]) {
+    for (const metric of metrics) {
+      tags.push(`${category}/${metric}`);
+    }
+  }
+
+  const data = await Promise.all(tags.map(async (tag) => {
+    return {
+      tag,
+      data: await downloadSingle(tag),
+    };
+  }));
+
+  let minSteps = Infinity;
+  for (const tag of data) {
+    minSteps = Math.min(minSteps, tag.data.length);
+  }
+
+  const labels: string[] = [ 'Wall time', 'Step' ].concat(tags);
+
+  const rows: Array<number[]> = [];
+  for (let i = 0; i < minSteps; i++) {
+    const newRow: number[] = data[0].data[i].slice(0, 2);
+    for (const tag of data) {
+      newRow.push(tag.data[i][2]);
+    }
+    rows.push(newRow);
+  }
+
+  const csv = labels.join(',') + '\n' +
+    rows.map((row) => row.join(',')).join('\n');
+  return csv;
+}
+
+download().then((csv) => {
+  process.stdout.write(csv);
+}).catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
---
+++ tools/chart-metrics.py
@@ -0,0 +1,40 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+category = 'train' if len(sys.argv) < 3 else sys.argv[2]
+
+with open(sys.argv[1]) as input:
+  reader = csv.reader(input, delimiter=',')
+  labels = next(reader)
+
+  steps = []
+  data = [ { 'label': label, 'values': [] } for label in labels[2:] ]
+  for row in reader:
+    steps.append(int(row[1]))
+    for value, subdata in zip(row[2:], data):
+      subdata['values'].append(float(value))
+
+  for subdata in data:
+    label = subdata['label']
+    if not category in label:
+      continue
+
+    if 'negative' in label:
+      color = '#0061ff' if '_50' in label else \
+          '#68a2ff' if '_25' in label or '_75' in label else \
+          '#89b6ff' if '_10' in label or '_90' in label else \
+          '#adccff'
+    else:
+      color = '#fc0036' if '_50' in label else \
+          '#ff7290' if '_25' in label or '_75' in label else \
+          '#ff9eb3' if '_10' in label or '_90' in label else \
+          '#ffc6d3'
+
+    values = np.array(subdata['values'])
+    plt.plot(steps, subdata['values'], color=color)
+
+  plt.title(category)
+  plt.grid(True)
+  plt.show()
