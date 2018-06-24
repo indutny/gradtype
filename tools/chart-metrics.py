d4ccaf37fcd685bd8a60d18d31860f80222dc65a
Fedor Indutny <fedor@indutny.com>
2018-06-24 18:15:33 -0400

    tools: questionably better style

--- tools/chart-metrics.py
+++ tools/chart-metrics.py
@@ -5,6 +5,7 @@ import sys
 
 category = 'train' if len(sys.argv) < 3 else sys.argv[2]
 
+ALPHA = 0.2
 NEGATIVE_COLOR = (1.0, 0.098039215686275, 0.223529411764706,)
 POSITIVE_COLOR = (0.219607843137255, 0.698039215686275, 0.317647058823529,)
 
@@ -13,37 +14,45 @@ with open(sys.argv[1]) as input:
   labels = next(reader)
 
   steps = []
-  data = [ { 'label': label, 'values': [] } for label in labels[2:] ]
+  data = {}
   for row in reader:
     steps.append(int(row[1]))
-    for value, subdata in zip(row[2:], data):
-      subdata['values'].append(float(value))
+    for value, label in zip(row[2:], labels[2:]):
+      if not category in label:
+        continue
 
-  for subdata in data:
-    label = subdata['label']
-    if not category in label:
-      continue
+      if not label in data:
+        data[label] = []
+      data[label].append(float(value))
 
-    values = np.array(subdata['values'])
+  rev_steps = steps.copy()
+  rev_steps.reverse()
+  fill_steps = steps + rev_steps
 
-    alpha = 1.0 if '_50' in label else \
-        0.75 if '_25' in label or '_75' in label else \
-        0.5 if '_10' in label or '_90' in label else \
-        0.25
+  def fill(forward, backward, color, alpha=1.0, label=None):
+    color += (alpha,)
+    backward_rev = backward.copy()
+    backward_rev.reverse()
+    plt.fill(fill_steps, forward + backward_rev,
+        color=color, label=label)
 
-    if 'positive' in label:
-      color = POSITIVE_COLOR + (alpha,)
-    else:
-      color = NEGATIVE_COLOR + (alpha,)
+  fill(data[category + '/positive_5'], data[category + '/positive_95'],
+      POSITIVE_COLOR, ALPHA)
+  fill(data[category + '/positive_10'], data[category + '/positive_90'],
+      POSITIVE_COLOR, ALPHA)
+  fill(data[category + '/positive_25'], data[category + '/positive_75'],
+      POSITIVE_COLOR, ALPHA)
+  plt.plot(steps, data[category + '/positive_50'], color=POSITIVE_COLOR,
+      label='positive')
 
-    if '_50' in label:
-      if 'positive' in label:
-        label = 'positive'
-      else:
-        label = 'negative'
-    else:
-      label = None
-    plt.plot(steps, values, color=color, label=label)
+  fill(data[category + '/negative_5'], data[category + '/negative_95'],
+      NEGATIVE_COLOR, ALPHA)
+  fill(data[category + '/negative_10'], data[category + '/negative_90'],
+      NEGATIVE_COLOR, ALPHA)
+  fill(data[category + '/negative_25'], data[category + '/negative_75'],
+      NEGATIVE_COLOR, ALPHA)
+  plt.plot(steps, data[category + '/negative_50'], color=NEGATIVE_COLOR,
+      label='negative')
 
   plt.title(category)
   plt.legend(loc='upper left')
