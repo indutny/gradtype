6fde229d8e1dd16f32d72951fa516376d1b9d27e
Fedor Indutny <fedor@indutny.com>
2019-05-24 00:23:53 +0200

    toosl: chart-metrics can display both plots

--- tools/chart-metrics.py
+++ tools/chart-metrics.py
@@ -9,25 +9,30 @@ ALPHA = 0.2
 NEGATIVE_COLOR = (1.0, 0.098039215686275, 0.223529411764706,)
 POSITIVE_COLOR = (0.219607843137255, 0.698039215686275, 0.317647058823529,)
 
-with open(sys.argv[1]) as input:
-  reader = csv.reader(input, delimiter=',')
-  labels = next(reader)
+fig = plt.figure(1, figsize=(8, 8))
 
-  steps = []
-  data = {}
-  for row in reader:
-    steps.append(int(row[1]))
-    for value, label in zip(row[2:], labels[2:]):
-      if not category in label:
-        continue
+for category in [ 'train', 'validate' ]:
+  with open(sys.argv[1]) as input:
+    reader = csv.reader(input, delimiter=',')
+    labels = next(reader)
 
-      if not label in data:
-        data[label] = []
-      data[label].append(float(value))
+    steps = []
+    data = {}
+    for row in reader:
+      steps.append(int(row[1]))
+      for value, label in zip(row[2:], labels[2:]):
+        if not category in label:
+          continue
+
+        if not label in data:
+          data[label] = []
+        data[label].append(float(value))
 
   rev_steps = steps.copy()
   rev_steps.reverse()
   fill_steps = steps + rev_steps
+
+  plt.subplot(2, 1, 1 if category is 'train' else 2)
 
   def fill(forward, backward, color, alpha=1.0, label=None):
     color += (alpha,)
@@ -51,10 +56,12 @@ with open(sys.argv[1]) as input:
       NEGATIVE_COLOR, ALPHA)
   fill(data[category + '/negative_25'], data[category + '/negative_75'],
       NEGATIVE_COLOR, ALPHA)
+
   plt.plot(steps, data[category + '/negative_50'], color=NEGATIVE_COLOR,
       label='negative')
 
   plt.title(category)
   plt.legend(loc='upper left')
   plt.grid(True)
-  plt.show()
+
+plt.show()
