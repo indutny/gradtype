01093fa2970b207a25518c09d126a6af96f883fa
Fedor Indutny <fedor@indutny.com>
2019-05-23 02:03:56 +0200

    tools: display both train/validate in roc

--- tools/roc.py
+++ tools/roc.py
@@ -4,27 +4,28 @@ import matplotlib.pyplot as plt
 import sklearn.metrics
 import sys
 
-category = 'train' if len(sys.argv) < 3 else sys.argv[2]
-
 with open(sys.argv[1]) as f:
-  data = json.load(f)
-  data = data[category]
-
-  positive = data['positives']
-  negative = data['negatives']
-
-  y_true = [ 1 ] * len(positive) + [ 0 ] * len(negative)
-  y_score = -np.array(positive + negative)
-
-  fpr, tpr, _ = sklearn.metrics.roc_curve(y_true, y_score)
-  auc = sklearn.metrics.auc(fpr, tpr)
-
   fig = plt.figure(1, figsize=(8, 8))
+  data = json.load(f)
 
-  plt.plot(fpr, tpr, color='green', label='AUC %0.3f' % auc)
+  for category in [ 'train', 'validate' ]:
+    sub_data = data[category]
+
+    positive = sub_data['positives']
+    negative = sub_data['negatives']
+
+    y_true = [ 1 ] * len(positive) + [ 0 ] * len(negative)
+    y_score = -np.array(positive + negative)
+
+    fpr, tpr, _ = sklearn.metrics.roc_curve(y_true, y_score)
+    auc = sklearn.metrics.auc(fpr, tpr)
+
+    plt.plot(fpr, tpr, color='green' if category is 'validate' else 'blue',
+        label='{}, AUC {:0.3f}'.format(category, auc))
+
+  plt.title(sys.argv[1])
+
   plt.xscale('log')
-
-  plt.title(category)
   plt.xlim([0.001, 1.0])
   plt.ylim([0.7, 1.0])
   plt.xlabel('False Positive Rate')
