a4f545fb71bbb614fd4fedd7c21d2ac795f688e8
Fedor Indutny <fedor@indutny.com>
2019-05-24 09:50:13 +0200

    train: fix reporting

--- tf/train.py
+++ tf/train.py
@@ -129,8 +129,7 @@ with tf.Session() as sess:
     for batch in train_batches:
       tensors = [ train, update_global_step_t, t_metrics, t_reg_loss,
           t_grad_norm ]
-      try:
-        _, _, metrics, reg_loss, grad_norm = sess.run(tensors, feed_dict={
+      train_feed = {
           holds: batch['holds'],
           codes: batch['codes'],
           deltas: batch['deltas'],
@@ -138,12 +137,15 @@ with tf.Session() as sess:
           categories: batch['categories'],
           category_mask: train_mask,
           training: True,
-        })
+        }
+      try:
+        _, _, metrics, reg_loss, grad_norm = sess.run(tensors,
+            feed_dict=train_feed)
       except tf.errors.InvalidArgumentError:
         # Catch NaN and inf global norm
         print('got invalid argument error, printing gradients')
         for (grad, var) in zip(unclipped_grads, variables):
-          print('{}: {}'.format(var.name, sess.run(grad)))
+          print('{}: {}'.format(var.name, sess.run(grad, feed_dict=train_feed)))
         raise
       metrics['regularization_loss'] = reg_loss
       metrics['grad_norm'] = grad_norm
