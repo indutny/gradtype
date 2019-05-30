2891775340eeaf05fefa197ab0eae842a8212828
Fedor Indutny <fedor@indutny.com>
2019-05-30 12:49:50 +0200

    train: drop LR after longer time

--- tf/train.py
+++ tf/train.py
@@ -85,7 +85,7 @@ else:
 with tf.variable_scope('optimizer'):
   t_lr = tf.constant(LR, dtype=tf.float32)
   if AUTO:
-    power = tf.floor(tf.cast(global_step_t, dtype=tf.float32) / 20000.0)
+    power = tf.floor(tf.cast(global_step_t, dtype=tf.float32) / 50000.0)
     power = tf.minimum(3.0, power)
     t_lr /= 10.0 ** power
     t_metrics['lr'] = t_lr
