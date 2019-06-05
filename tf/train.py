589b161d0e1eb3655bac0b9d5012555321793f2e
Fedor Indutny <fedor@indutny.com>
2019-06-05 16:25:05 +0200

    train: fix

--- tf/train.py
+++ tf/train.py
@@ -187,7 +187,7 @@ with tf.Session() as sess:
     log_summary('train', metrics, step)
 
     grad_clip_value *= (1.0 - grad_clip_lambda)
-    grad_clip_value += grad_clip_lambda * metrics['grad_norm'])
+    grad_clip_value += grad_clip_lambda * metrics['grad_norm']
 
     end_time = time.time()
     print('Mean batch time: {}'.format(
