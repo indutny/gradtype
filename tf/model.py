2eb76140294c202a8e11ea3b86d0de951194df2c
Fedor Indutny <fedor@indutny.com>
2019-06-15 14:05:05 -0700

    model: fix

--- tf/model.py
+++ tf/model.py
@@ -122,6 +122,7 @@ class Model():
     seq_index = tf.expand_dims(tf.range(max_sequence_len), axis=0,
         name='seq_index')
     mask = seq_index < tf.expand_dims(sequence_len, axis=-1)
+    mask = tf.cast(mask, dtype=tf.float32)
     mask /= tf.reduce_sum(mask, axis=-1, keepdims=True, name='mask_sum') + 1e-23
     mask = tf.expand_dims(mask, axis=-1, name='expanded_mask')
 
