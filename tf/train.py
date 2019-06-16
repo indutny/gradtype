2087d9c6afea7ee3522f6b1eb2ac96f5a9374c03
Fedor Indutny <fedor@indutny.com>
2019-06-15 22:09:43 -0700

    train: add reference

--- tf/train.py
+++ tf/train.py
@@ -16,6 +16,7 @@ RESTORE_FROM = os.environ.get('GRADTYPE_RESTORE')
 LOG_DIR = os.path.join('.', 'logs', RUN_NAME)
 SAVE_DIR = os.path.join('.', 'saves', RUN_NAME)
 
+# See https://arxiv.org/pdf/1511.06807.pdf
 GRAD_NOISE_GAMMA = 0.55
 GRAD_NOISE_ETA = 1.0
 
